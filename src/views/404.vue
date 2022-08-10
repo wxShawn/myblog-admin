0c6e5d2bad9b4b824e531ef16ae2c85e1add6311
wxShawn <1806394760@qq.com>
2022-08-10 14:06:13 +0800

    替换404页面图标

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,9 @@
 <template>
   <div style="height: 100%; display: flex; justify-content: center; align-items: center;">
     <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
+      <template #icon>
+        <img src="../assets/Page_Not_Found_Outline.svg" alt="404图片">
+      </template>
       <template #footer>
         <n-button @click="gotoHome">前往首页</n-button>
       </template>
